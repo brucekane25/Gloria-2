8e16ff39c269739b2c9dda34891bb0d5faaede0d
brucekane25 <133052604+brucekane25@users.noreply.github.com>
2024-12-24 07:59:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
   const [currentPage, setCurrentPage] = useState(1);
   const [pages, setPages] = useState(1);
   const [filterpages, setFilterPages] = useState(0);
-  const [limit, setLimit] = useState(5000);
+  const [limit, setLimit] = useState(4500);
   const [selectedCategory, setSelectedCategory] = useState("");
   const [randomEvents, setRandomEvents] = useState([]);
   const [totalEvents, setTotalEvents] = useState(null); 
