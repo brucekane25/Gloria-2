af028f9dfe3cdf36866555ff0765bdbd1499bc5c
Belal Khan <133052604+brucekane25@users.noreply.github.com>
2025-04-01 22:59:06 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 // const back = import.meta.env.VITE_BACK
 // Base configuration for Axios
 const apiClient = axios.create({
-  baseURL: 'https://historia-backend-2.onrender.com/api/events',
+  
+  baseURL: 'https://historiabackend-production.up.railway.app/api/events',
+  // baseURL: 'https://historia-backend-2.onrender.com/api/events',
 // baseURL: `${back}api/events`, // Replace with your backend URL
   // baseURL: 'http://localhost:5000/api/events', // Replace with your backend URL
   headers: {
